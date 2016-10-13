bd38340a539d0f069fdcbb4f5f2db16905c0dafb
Jiri Kuncar <jiri.kuncar@gmail.com>
2016-10-13 23:38:45 +0200

    cli: wait for detector events
    Signed-off-by: Jiri Kuncar <jiri.kuncar@gmail.com>

--- cosmicpi_protocol/__init__.py
+++ cosmicpi_protocol/__init__.py
@@ -1,12 +1,20 @@
 """cosmicpi-protocol - Parser for data read from Arduino."""
 
+import time
+from threading import Thread, Timer
+try:
+    from Queue import Queue
+except ImportError:
+    from queue import Queue
+
 import pkg_resources
 import yaml
+from werkzeug.exceptions import NotFound
 from werkzeug.routing import Map, Rule
 
 __version__ = '0.1.0'
 __author__ = 'Jiri Kuncar <jiri.kuncar@gmail.com>'
-__all__ = ()
+__all__ = ('Detector', 'Parser')
 
 
 def yield_protocols():
@@ -15,6 +23,12 @@ def yield_protocols():
         yield group.name, pkg_resources.resource_filename(
             group.module_name, '{0}.yaml'.format(group.name)
         )
+
+def load_events():
+    """Load defaut events configuration."""
+    return yaml.load(pkg_resources.resource_stream(
+        'cosmicpi_protocol', 'events.yaml'
+    ))
 
 class Parser(object):
     """Parser of messages defined in protocol."""
@@ -34,3 +48,62 @@ class Parser(object):
     def match(self, message):
         """Match incomming message and return tuple (endpoint, data)."""
         return self.matcher.match(message)
+
+
+class Detector(object):
+    """Detector controller."""
+
+    def __init__(self, events=None, status=None):
+        """Create a detector state."""
+        self.events = events or load_events()
+        self.status = status or {}
+        self.queue = Queue()
+
+    def clock(self):
+        """Start timers."""
+        def worker(period, keys):
+            """Define periodic task."""
+            while True:
+                time.sleep(period)
+                event = self.prepare_event(*keys)
+                if event:
+                    self.queue.put(event)
+
+        for timers in self.events.get('timers', []):
+            for period, keys in timers.items():
+                t = Thread(target=worker, args=(int(period), keys))
+                t.daemon = True
+                t.start()
+
+
+    def prepare_event(self, *keys):
+        """Build output from status keys."""
+        keys = set(keys)
+        return {
+            key: value for key, value in self.status.items() if key in keys
+        }
+
+    def update(self, identifier, data):
+        """Update status with new data for given identifier."""
+        protocol, event = identifier.split(':', 1)
+        self.status[event] = data
+        if event in self.events.get('triggers', {}):
+            keys = self.events['triggers'][event]
+            self.queue.put(self.prepare_event(*keys))
+
+    def read(self, input_, parser=None):
+        """Read and parse input."""
+        parser = parser or Parser()
+
+        self.clock()
+
+        for line in input_.readlines():
+            if not line:
+                break
+            try:
+                self.update(*parser.match(line.strip()))
+            except NotFound:
+                pass
+                # TODO add logging
+
+        self.queue.join()
--- cosmicpi_protocol/__main__.py
+++ cosmicpi_protocol/__main__.py
@@ -6,13 +6,18 @@ import sys
 import click
 from werkzeug.exceptions import NotFound
 
-from . import Parser
+from . import Detector, Parser
 
 
-@click.command()
+@click.group()
+def cli():
+    """Command line interface for testing CosmicPi."""
+
+
+@cli.command()
 @click.argument('input_', type=click.File('rb'), default='-')
 @click.argument('output', type=click.File('wb'), default='-')
-def cli(input_, output):
+def parse(input_, output):
     """Parse input file and print JSON output."""
     parser = Parser()
     for line in input_.readlines():
@@ -25,6 +30,32 @@ def cli(input_, output):
                            file=sys.stderr)
 
             output.flush()
+
+
+@cli.command()
+@click.argument('input_', type=click.File('rb'), default='-')
+@click.argument('output', type=click.File('wb'), default='-')
+def run(input_, output):
+    """Run simple event agregator."""
+    from threading import Thread, Timer
+
+    detector = Detector()
+
+    def worker():
+        while True:
+            item = detector.queue.get()
+            output.write(json.dumps(item))
+            output.write('\n')
+            output.flush()
+            detector.queue.task_done()
+
+    number_worker_threads = 1
+    for i in range(number_worker_threads):
+        t = Thread(target=worker)
+        t.daemon = True
+        t.start()
+
+    detector.read(input_)
 
 
 if __name__ == '__main__':  # pragma: no cover
